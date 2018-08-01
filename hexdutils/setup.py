0da991be3be6c40f649b90fbb9ceee3d8d703d24
Davide <davide_fiorito@libero.it>
2018-08-02 00:44:41 +0200

    setup.py variables added

--- hexdutils/setup.py
+++ hexdutils/setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as readmefile:
+    readme_description = readmefile.read()
+
 setup(
     name = "hexdutils",
     url = "https://github.com/davix3f/hexutils",
@@ -9,5 +12,8 @@ setup(
     author_email = "davide_fiorito@libero.it",
     version = "1.2",
     packages = find_packages(),
+    long_description = readme_description,
+    long_description_content_type = "text/markdown",
+    license = "Apache 2.0",
     include_package_data = False
 )
