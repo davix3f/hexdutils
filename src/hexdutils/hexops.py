03ce3747766a229db44ec8bf36516b1873eac2c9
Davide <davide_fiorito@libero.it>
2019-09-14 01:03:39 +0200

    divide->truediv; logging edited

--- src/hexdutils/hexops.py
+++ src/hexdutils/hexops.py
@@ -8,67 +8,66 @@ from hextoint import _hextoint as hextoint
 # +
 def _hex_add(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
     if (type(first) is not str) or (type(second) is not str):
-        print("#1 arg type:", type(first), "\n#2 arg type:", type(second))
-        raise TypeError("Value type must be str. Use intohex(int value)")
+        raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
     result = hextoint(first) + hextoint(second)
     if hex_output:
         return(intohex(result, hex_output_prefix, hex_output_upper))
     else:
         return(result)
 
+
 # -
-def _hex_subtract(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
+def _hex_sub(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
     if (type(first) is not str) or (type(second) is not str):
-        raise TypeError("Value type must be str. Use intohex(int value)")
+        raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
     result = hextoint(first) - hextoint(second)
     if hex_output:
         return(intohex(result, hex_output_prefix, hex_output_upper))
     else:
         return(result)
 
+
 # *
-def _hex_multiply(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
+def _hex_mul(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
     if (type(first) is not str) or (type(second) is not str):
-        print("#1 arg type:", type(first), "\n#2 arg type:", type(second))
-        raise TypeError("Value type must be str. Use intohex(int value)")
+        raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
     result = hextoint(first) * hextoint(second)
     if hex_output:
         return(intohex(result, hex_output_prefix, hex_output_upper))
     else:
         return(result)
 
-# /
-def _hex_divide(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
-    if (type(first) is not str) or (type(second) is not str):
-        raise TypeError("Value type must be str. Use intohex(int value)")
-    result = hextoint(first) / hextoint(second)
-    if hex_output:
-        return(intohex(result, hex_output_prefix, hex_output_upper))
-    else:
-        return(result)
 
 # //
-def _hex_floor(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
+def _hex_floordiv(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
     if (type(first) is not str) or (type(second) is not str):
-        raise TypeError("Value type must be str. Use intohex(int value)")
+        raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
     result = hextoint(first) // hextoint(second)
     if hex_output:
         return(intohex(result, hex_output_prefix, hex_output_upper))
     else:
         return(result)
+
+
+# /
+_hex_truediv = _hex_floordiv
+
+
 # %
 def _hex_mod(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
         if (type(first) is not str) or (type(second) is not str):
-            raise TypeError("Value type must be str. Use intohex(int value)")
+            raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
         result = hextoint(first) % hextoint(second)
         if hex_output:
             return(intohex(result, hex_output_prefix, hex_output_upper))
         else:
             return(result)
+
+
 # **
 def _hex_power(first, second, hex_output, hex_output_prefix=False, hex_output_upper=False):
     if (type(first) is not str) or (type(second) is not str):
-        raise TypeError("Value type must be str. Use intohex(int value)")
+        raise TypeError("Values type must be str (are %s). Use intohex(int value)" % [type(first), type(second)])
     result = hextoint(first) ** hextoint(second)
     if hex_output:
         return(intohex(result, hex_output_prefix, hex_output_upper))
