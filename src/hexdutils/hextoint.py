cb8def8818919b92aa992a5a206c6356e126618b
Davide <davide_fiorito@libero.it>
2019-09-14 01:04:17 +0200

    Added support to negative numbers

--- src/hexdutils/hextoint.py
+++ src/hexdutils/hextoint.py
@@ -7,20 +7,28 @@ from __hex_constants__ import __hex_letters, __alphabet
 
 def _hextoint(target):
     if type(target) is not str:
-        raise TypeError("Argument must be str")
+        raise TypeError("Argument must be str (is %s)" % type(target))
     decimals = []
+    isneg_mult = None
+    if target[0] == "-":
+        isneg_mult = -1
+        target = target[1:]
+    else:
+        isneg_mult = 1
     if target[:2] is "0x":
         target = target[2:]
     power_of_sixteen = len(target)-1
     for item in target:
         try:
             decimals.append(int(item)*(16**power_of_sixteen))
+            power_of_sixteen -= 1
         except:
+            # if item is a letter
             if item.lower() in __hex_letters:
                 decimals.append(__hex_letters[item.lower()]*(16**power_of_sixteen))
+                # Letter correspondant value * 16**location in list (= n_th figure)
             else:
+                # not a,b,c,d,e,f
                 raise ValueError(item, "doesn't belong to hex system")
-
-        power_of_sixteen -= 1
     else:
-        return(sum(decimals))
+        return(sum(decimals) * isneg_mult)
