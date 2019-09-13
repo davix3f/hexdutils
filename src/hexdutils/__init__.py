c9ddbe3fa33f22427e63d3c7474e708abf28d0af
Davide <davide_fiorito@libero.it>
2019-09-14 01:01:49 +0200

    +hexclass->hexobj; renaming

--- src/hexdutils/__init__.py
+++ src/hexdutils/__init__.py
@@ -11,9 +11,11 @@ from hextoint import _hextoint as hextoint
 from hexabc import _abctohex as abctohex, _hextoabc as hextoabc
 
 from hexops import _hex_add as hex_add,\
-                   _hex_subtract as hex_subtract,\
-                   _hex_multiply as hex_multiply,\
-                   _hex_divide as hex_divide,\
-                   _hex_floor as hex_floor,\
+                   _hex_sub as hex_sub,\
+                   _hex_mul as hex_mul,\
+                   _hex_truediv as hex_truediv,\
+                   _hex_floordiv as hex_floordiv,\
                    _hex_mod as hex_mod,\
                    _hex_power as hex_power
+
+from hexclass import hexobj
